3becdf5419512f5e82aac495975b024f039da7ce
Calabonga <dev@calabonga.net>
2020-09-26 14:03:54 +0700

    MicroserviceSaveChangesException added

---
+++ src/Calabonga.Microservices.Core/Calabonga.Microservices.Core/Exceptions/MicroserviceSaveChangesException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Calabonga.Microservices.Core.Exceptions
+{
+    /// <summary>
+    /// Represent ArgumentNull Exception
+    /// </summary>
+    [Serializable]
+    public class MicroserviceSaveChangesException : Exception
+    {
+        public MicroserviceSaveChangesException() : base(AppContracts.Exceptions.ArgumentNullException)
+        {
+
+        }
+
+        public MicroserviceSaveChangesException(string message) : base(message)
+        {
+
+        }
+
+        public MicroserviceSaveChangesException(string message, Exception exception) : base(message, exception)
+        {
+
+        }
+
+        public MicroserviceSaveChangesException(Exception exception) : base(AppContracts.Exceptions.ArgumentNullException, exception)
+        {
+
+        }
+    }
+}
