ad89a4027691c80d27a3bd4ec950eac247b2cebd
Calabonga <dev@calabonga.net>
2023-06-08 14:26:02 +0300

    Some unit-tests added

--- src/Calabonga.Microservices.Core/Calabonga.Microservices.Core.Tests/StringExtensionsTests.cs
+++ src/Calabonga.Microservices.Core/Calabonga.Microservices.Core.Tests/StringExtensionsTests.cs
@@ -5,6 +5,52 @@ namespace Calabonga.Microservices.Core.Tests;
 public class StringExtensionsTests
 {
     [Fact]
+    public void It_IsNullOrEmpty_should_returns_true_when_not_empty()
+    {
+        // arrange
+        var testString = "some text";
+        var expected = false;
+
+        // act
+        var actual = testString.IsNullOrEmpty();
+
+
+        // assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void It_IsNullOrEmpty_should_returns_false_when_not_empty()
+    {
+        // arrange
+        const string testString = "";
+        const bool expected = true;
+
+        // act
+        var actual = testString.IsNullOrEmpty();
+
+
+        // assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void It_IsNullOrEmpty_should_returns_false_when_not_null()
+    {
+        // arrange
+        const string testString = null;
+        const bool expected = true;
+
+        // act
+        var actual = testString.IsNullOrEmpty();
+
+
+        // assert
+        Assert.Equal(expected, actual);
+    }
+
+
+    [Fact]
     public void It_should_returns_true_when_not_empty()
     {
         // arrange
